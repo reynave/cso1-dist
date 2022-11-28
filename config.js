85bc04f583ec77cf45e4056be5b8935662e19d59
reynave <felix.renaldi@gmail.com>
2022-11-28 18:35:05 +0700

    update

--- config.js
+++ config.js
@@ -1,5 +1,13 @@
+
 var env_api = "http://194.233.77.27/cso/api/";
 var env_public = ""; 
 var env_base_url ="http://194.233.77.27/cso/admin/#/";
 var env_socket = 'http://194.233.77.27:3000';
 var env_terminal1 = ' "http://194.233.77.27/cso/terminal1/#/"';
+
+
+// var env_api = "http://192.168.202.72/cso1-api/";
+// var env_public = ""; 
+// var env_base_url ="http://192.168.202.72/cso-dist/admin/#/";
+// var env_socket = 'http://192.168.202.72:3000';
+// var env_terminal1 = ' "http://192.168.202.72/cso-dist/www/#/"';
